2afecd2dcbb0060f342e1cbf7a31d729799cb065
ShivamMainro <shivammainro3@gmail.com>
2025-08-19 02:05:44 +0530

    Added email file sharing feature

--- routes/files.js
+++ routes/files.js
@@ -44,6 +44,46 @@ router.post('/',(req,res)=>{
 
 
     // Response -> Link
-})
+});
+
+router.post('/send', async (req, res) => {
+  const { uuid, emailTo, emailFrom, expiresIn } = req.body;
+  if(!uuid || !emailTo || !emailFrom) {
+      return res.status(422).send({ error: 'All fields are required except expiry.'});
+  }
+
+  // Db se data 
+  try {
+    const file = await File.findOne({ uuid: uuid });
+    if(file.sender) {
+      return res.status(422).send({ error: 'Email already sent once.'});
+    }
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+    const response = await file.save();
+    
+    // send mail
+    const sendMail = require('../services/mailService');
+    sendMail({
+      from: emailFrom,
+      to: emailTo,
+      subject: 'inShare file sharing',
+      text: `${emailFrom} shared a file with you.`,
+      html: require('../services/emailTemplate')({
+                emailFrom, 
+                downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email` ,
+                size: parseInt(file.size/1000) + ' KB',
+                expires: '24 hours'
+            })
+    }).then(() => {
+      return res.json({success: true});
+    }).catch(err => {
+      return res.status(500).json({error: 'Error in email sending.'});
+    });
+} catch(err) {
+  return res.status(500).send({ error: 'Something went wrong.'});
+}
+
+});
 
 module.exports = router;
